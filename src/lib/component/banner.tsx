94fce1872d07d3cb3cbcc7464dd4ff9c0cef3483
혀느현스 <hyuns@hyuns.dev>
2022-02-28 00:27:32 +0900

    textField 제작중

---
+++ src/lib/component/banner.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from "swiper";
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const Divver = styled.div`
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+`
+
+const BannerImg = styled.img`
+    width: 100%;
+    max-width: 600px;
+    max-height: 90px;
+    border-radius: 8px;
+`
+
+interface BannerItem {
+    img: string;
+    to: string;
+}
+
+interface Banner {
+    banners: BannerItem[];
+}
+
+export function Banner(props:Banner) {
+    return (
+        <Divver>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={8}
+                pagination={{
+                    clickable: true,
+                }}
+                loop={true}
+                modules={[Pagination, Autoplay]}
+                autoplay={{ delay: 5000 }}
+            >
+                {
+                    props.banners.map((e:BannerItem, i:number) => {
+                        if (e.to.includes('http')) {
+                            return (
+                                <SwiperSlide key={i}>
+                                    <a href={e.to || "/"} target={'_blank'} rel="noreferrer">
+                                        <BannerImg src={e.img} alt="" />
+                                    </a>
+                                </SwiperSlide>
+                            )
+                        } else {
+                            return (
+                                <SwiperSlide key={i}>
+                                    <Link to={e.to || "/"}>
+                                        <BannerImg src={e.img} alt="" />
+                                    </Link>
+                                </SwiperSlide>
+                            )
+                        }
+                    }
+                    )
+                }
+            </Swiper>
+        </Divver>
+    )
+}
+
+Banner.defaultProps = {
+    
+}
---
+++ src/stories/input/button.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import { Button } from '../../lib';
+
+export default {
+  title: 'button/Button',
+  component: Button,
+  argTypes: {
+  },
+} as Meta
+
+const Template: Story = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'text'
+};
